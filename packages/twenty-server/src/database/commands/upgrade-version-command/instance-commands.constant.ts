926f0c29327863592152defa0f17e530e1b14949
Mike Famularo <215557151+mfamularopsyc@users.noreply.github.com>
2026-06-30 14:07:56 -0400

    Fix locked view upgrade command version

--- packages/twenty-server/src/database/commands/upgrade-version-command/instance-commands.constant.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/instance-commands.constant.ts
@@ -40,8 +40,8 @@ import { MakePublicDomainApplicationIdNotNullSlowInstanceCommand } from 'src/dat
 import { CreateDpaAgreementCoreTableFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-17/2-17-instance-command-fast-1801000020000-create-dpa-agreement-core-table';
 import { CreateApplicationTranslationCoreTableFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-17/2-17-instance-command-fast-1801000100000-create-application-translation-core-table';
 import { AddTsVectorFieldMetadataIdToSearchFieldMetadataFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-18/2-18-instance-command-fast-1810000001000-add-ts-vector-field-metadata-id-to-search-field-metadata';
+import { AddIsLockedToViewFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-18/2-18-instance-command-fast-1810000002000-add-is-locked-to-view';
 import { BackfillTsVectorFieldMetadataIdOnSearchFieldMetadataSlowInstanceCommand } from 'src/database/commands/upgrade-version-command/2-18/2-18-instance-command-slow-1810000003000-backfill-ts-vector-field-metadata-id-on-search-field-metadata';
-import { AddIsLockedToViewFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-19/2-19-instance-command-fast-1802000000000-add-is-locked-to-view';
 import { AddCacheTokensToAgentChatThreadFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-2/2-2-instance-command-fast-1777455269302-add-cache-tokens-to-agent-chat-thread';
 import { AddLogoToApplicationFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-2/2-2-instance-command-fast-1777539664664-add-logo-to-application';
 import { AddSubFieldNameToViewSortEarlyFastInstanceCommand } from 'src/database/commands/upgrade-version-command/2-3/2-3-instance-command-fast-1747234200000-add-sub-field-name-to-view-sort';
@@ -170,6 +170,6 @@ export const INSTANCE_COMMANDS = [
   CreateDpaAgreementCoreTableFastInstanceCommand,
   CreateApplicationTranslationCoreTableFastInstanceCommand,
   AddTsVectorFieldMetadataIdToSearchFieldMetadataFastInstanceCommand,
-  BackfillTsVectorFieldMetadataIdOnSearchFieldMetadataSlowInstanceCommand,
   AddIsLockedToViewFastInstanceCommand,
+  BackfillTsVectorFieldMetadataIdOnSearchFieldMetadataSlowInstanceCommand,
 ];
